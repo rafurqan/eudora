77420cb712ae94b4d3579d5144f59be7bbfc29fc
Rahmatullah furqan <rahmatullahfurqan@gmail.com>
2025-04-17 13:49:30 +0700

    add requirement sh

--- requirement.sh
+++ requirement.sh
@@ -1,17 +1,8 @@
 # Update apt repository dan install dependencies PHP
-sudo apt-get update
-sudo apt-get install -y php php-cli php-fpm php-mbstring php-xml php-curl php-zip php-mysql unzip curl
+apt-get update
+apt-get install -y php php-cli php-fpm php-mbstring php-xml php-curl php-zip php-mysql unzip curl
 
-# Install Composer
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
-
-# Pastikan Composer telah terinstal dengan benar
-composer --version
-
-# Install dependencies Laravel (jika belum ada file vendor/)
-cd /path/to/your/laravel/project
-composer install --no-dev --optimize-autoloader
+npm install
 
 # Set up Laravel
 php artisan key:generate
