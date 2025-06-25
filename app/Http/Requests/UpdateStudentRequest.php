f24fdb8910c0ec8707c9b8fce6896cff540f0a65
Rahmatullah furqan <rahmatullahfurqan@gmail.com>
2025-06-25 19:49:52 +0700

    bug : update student

--- app/Http/Requests/UpdateStudentRequest.php
+++ app/Http/Requests/UpdateStudentRequest.php
@@ -3,6 +3,7 @@ namespace App\Http\Requests;
 
 use App\Rules\Base64File;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateStudentRequest extends FormRequest
 {
@@ -14,7 +15,7 @@ class UpdateStudentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'registration_code' => 'required|string|unique:students,registration_code|max:50',
+            'registration_code' => ['required|string|unique:students,registration_code|max:50', Rule::unique('students', 'registration_code')->ignore($this->student->id)],
             'full_name' => 'required|string|max:100',
             'nickname' => 'nullable|string|max:50',
             'religion.id' => 'nullable|uuid|exists:religions,id',
