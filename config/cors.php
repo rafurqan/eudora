d7763bbb8bac22a4e6fbda009da3a1ce5374cdda
Rahmatullah furqan <rahmatullahfurqan@gmail.com>
2025-07-22 09:39:01 +0700

    adjust table

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://xtudents.test', 'http://invoker.test', 'http://localhost:5173', 'http://zeus.railway.internal:8080', 'https://zeus-production-0313.up.railway.app', 'http://0.0.0.4173'],
+    'allowed_origins' => ['http://xtudents.test', 'http://invoker.test', 'http://localhost:5173', 'http://zeus.railway.internal:8080', 'https://zeus-production-0313.up.railway.app', 'http://0.0.0.4173','https://zeus-1owu.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
