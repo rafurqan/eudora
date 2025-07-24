52c7e10ab3cc8f00b8674ff402dac9b68b55b40d
Rahmatullah furqan <rahmatullahfurqan@gmail.com>
2025-07-24 19:38:21 +0700

    bug fix

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\Master\CityController;
 use App\Http\Controllers\API\Master\ContactTypeController;
 use App\Http\Controllers\API\Master\IncomeRangeController;
+use App\Http\Controllers\API\Master\OccupationController;
 use App\Http\Controllers\API\Master\ParentTypeController;
 use App\Http\Controllers\API\Master\ProgramController;
 use App\Http\Controllers\API\Master\ProvinceController;
@@ -86,7 +87,7 @@ Route::prefix('v1')->group(function () {
             Route::apiResource('cities', CityController::class)->only(['index']);
             Route::apiResource('provinces', ProvinceController::class)->only(['index']);
             Route::apiResource('parent-types', ParentTypeController::class)->only(['index', 'store', 'update', 'destroy']);
-            Route::apiResource('occupations', ParentTypeController::class)->only(['index', 'store', 'update', 'destroy']);
+            Route::apiResource('occupations', OccupationController::class)->only(['index', 'store', 'update', 'destroy']);
 
             // Master Biaya
             Route::apiResource('services', ServiceController::class)->only(['index', 'store', 'update', 'destroy']);
