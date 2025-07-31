542b654b6bb2173cd2eb5e02d8baf91d520a77da
Rahmatullah furqan <rahmatullahfurqan@gmail.com>
2025-07-31 13:52:17 +0700

    update file

--- config/app.php
+++ config/app.php
@@ -123,4 +123,8 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    'aliases' => [
+        'Cloudinary' => CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary::class,
+    ],
+
 ];
